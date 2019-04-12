555cf01bd03b472d53d1393e002df94eee8acd71
Padam Sethia <padamsethia5@gmail.com>
2019-04-12 10:47:19 +0530

    Updated

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask , request , jsonify
+from flask import Flask , request , jsonify ,json
 import os
 import subprocess 
-
+import requests
 
 app = Flask(__name__)
 
 
 UPLOAD_FOLDER = './sqldump'
 log_line = '{STATUS} : {DESC}'
+_dumpfile = 'sqldump.sql'
+
 @app.route('/api/sqldump' , methods = ['POST' , 'GET'])
 def get_sqldump():
     if request.method == 'POST':
@@ -20,20 +22,24 @@ def get_sqldump():
             print(log_line.format('INFO' , 'FIle not selected properly , or empty file being supplied'))
             return jsonify({'message' : 'Empty file is ebing submitted'})
         if file:
-            file.save(os.path.join(UPLOAD_FOLDER , file.filename))
-            return jsonify({'message' : 'Filed saved under ./sqldump as {}'.format(file.filename) , 'status' : 'success' })
+            file.save(os.path.join(UPLOAD_FOLDER , ))
+            return jsonify({'message' : 'Filed saved under ./sqldump as {}'.format(_dumpfile) , 'status' : 'success' })
 
 
-@app.route('/api/nlp2sql/<input>' , methods = ['POST'])
-def nlp2sql(input):
-    input_text = str(input)
+@app.route('/api/nlp2sql' , methods = ['POST' , "GET"])
+def nlp2sql():
+    
+    input_text = request.get_json()
     # Logic for In2Sql 
-    if input_text:
-        _dbsql = ""
-        _query = input_text
-        sql_result = suprocess.call(["python3 -m ln2sql.main -d {database} -l lang_store/english.csv -j output.json -i {query}".format( _dbsql , _query)])
+    xx = '"'+str(input_text['text'])+'"'
+               
+    query = "python -m ln2sql.main -d database_store\\{0} -l lang_store/english.csv -j output.json -i {1}".format( _dumpfile , xx)
+    print(query)
+    
+    sql_result = subprocess.call(query)
+    # print(sql_result)
 
-    return jsonify({'message' : 'Executed SQL results' , 'query' : sql_result})
+    return jsonify({'message' : 'Executed SQL results' , 'query' : "Check the output.json file"})
 
 # python3 -m ln2sql.main -d database_store/city.sql -l lang_store/english.csv -j output.json -i "Count how many city there are with the name blob?"
 
@@ -44,9 +50,23 @@ def home():
     # Take SQL dump from the user and upload it to /api/sqldump -> Single user app!
     # Give user a few tips on how to navigate 
     # Build chat interface for app
+    if request.method == "POST":
+        pass
 
 
-# RASA NLU chat code :
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+# RASA NLU chat code
